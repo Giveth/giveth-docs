c9fa914060a0a652652b5f8755cf6b5f37b5c6b9
geleeroyale <admin@geleeroyale.work>
2021-01-19 00:02:36 +0100

    fix mitches situation

--- sidebarsGuides.js
+++ sidebarsGuides.js
@@ -1,5 +1,6 @@
 module.exports = {
   guides: {
-    Guides: ['whatisgiveth', 'gettingstarted', 'createproject', 'troubleshooting', 'crypto']
+Guides: ['whatisgiveth', 'gettingstarted', 'createproject', 'troubleshooting', 'crypto']
+
   }
 }
