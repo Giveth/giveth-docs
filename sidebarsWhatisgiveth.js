28eb380b77bf4c6fb8de585256176cfa9e231a7a
Franco <franculio.ruiz@gmail.com>
2023-07-13 15:47:24 +0200

    Adding Zero-Fees doc

--- sidebarsWhatisgiveth.js
+++ sidebarsWhatisgiveth.js
@@ -1,6 +1,6 @@
 module.exports = {
   whatisgiveth: [
-     'whatisgiveth','covenant','codeofconduct','communityCommsGuide', 'conflictResolution','externalContributors',
+     'whatisgiveth','zero-fees', 'covenant','codeofconduct','communityCommsGuide', 'conflictResolution','externalContributors',
       {
         type: 'category',
         label: 'Circles',
